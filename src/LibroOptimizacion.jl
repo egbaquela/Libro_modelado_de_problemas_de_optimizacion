a05b121a164bfe0f3e5390c18ce2fde2eda7f1ff
Enrique Gabriel Baquela <egbaquela@hotmail.com>
2025-11-22 08:26:05 -0300

    Updating files

--- src/LibroOptimizacion.jl
+++ src/LibroOptimizacion.jl
@@ -24,7 +24,7 @@ include("data.jl")
 This function is called during CI.
 """
 function build()
-    println("Building Example Book")
+    println("Building Book")
     # To avoid publishing broken websites.
     fail_on_error = true
     gen(; fail_on_error)
